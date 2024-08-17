94c711e1dc70ee168db07159dc1528309430a06e
Mikhail Ponomarev <mikhail.ponomarev@raiffeisen.ru>
2024-08-17 16:12:04 +0300

    feat: Impl loader

--- index.js
+++ index.js
@@ -1,39 +1,31 @@
-const getDogs = async () => {
-  let result;
-  let links;
-  try {
-    result = await fetch("https://dog.ceo/api/breeds/image/random/6");
-  } catch (error) {
-    console.log(error);
-  }
-  if (result.ok) {
-    const body = await result.json();
-    links = body.message;
-  } else {
-    throw new Error(`Response status is ${result.status}`);
-  }
-  return links;
+const displayPictures = (links) => {
+    const container = document.querySelector('.pictures-container');
+    links.forEach((elem) => {
+        const img = document.createElement('img');
+        img.className = 'picture';
+        img.src = elem;
+        img.alt = 'Some dog';
+        img.width = 200;
+        img.height = 190;
+        container.append(img);
+    });
 };
 
-const appendPictures = async () => {
-  const dogs = await getDogs();
-  const container = document.querySelector(".pictures-container");
-  dogs.forEach((elem) => {
-    const img = document.createElement("img");
-    img.className = "picture";
-    img.src = elem;
-    img.alt = "Some dog";
-    img.width = 200;
-    img.height = 190;
-    container.append(img);
-  });
+const fetchPictures = async () => {
+    const loader = document.querySelector('.loader');
+    loader.style.display = 'flex';
+    try {
+        const response = await fetch('https://dog.ceo/api/breeds/image/random/6');
+        if (!response.ok) {
+            throw new Error(`Response status is ${result.status}`);
+        }
+        const body = await response.json();
+        displayPictures(body.message);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        loader.style.display = 'none';
+    }
 };
 
-document.querySelector(".get-btn").addEventListener("click", async () => {
-  await appendPictures();
-  document.querySelector(".get-more-btn").style.display = "block";
-});
-
-document
-  .querySelector(".get-more-btn")
-  .addEventListener("click", appendPictures);
+document.querySelector('.get-btn').addEventListener('click', fetchPictures);
